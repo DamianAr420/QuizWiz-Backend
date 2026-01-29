42f82ee0e2a0a7ebe723ee4e6c55c99aabe6428f
DamianAr420 <damianarg2115@gmail.com>
2026-01-29 17:42:53 +0100

    feat(stats)

---
+++ QuizWiz-Backend/Controllers/StatsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuizWiz_Backend.Data;
+using QuizWiz_Backend.DTOs;
+
+namespace QuizWiz_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public StatsController(AppDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<ActionResult<StatsDto>> GetGeneralStats()
+        {
+            var quizzesCount = await _context.Quizzes.CountAsync();
+            var questionsCount = await _context.Questions.CountAsync();
+            var usersCount = await _context.Users.CountAsync();
+
+            return Ok(new StatsDto(quizzesCount, questionsCount, usersCount));
+        }
+    }
+}
---
+++ QuizWiz-Backend/DTOs/StatsDto.cs
@@ -0,0 +1,4 @@
+﻿namespace QuizWiz_Backend.DTOs
+{
+    public record StatsDto(int TotalQuizzes, int TotalQuestions, int TotalUsers);
+}
