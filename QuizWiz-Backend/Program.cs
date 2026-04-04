020fe52e7733fb31dec73961db82deaf11d2ba32
DamianAr420 <damianarg2115@gmail.com>
2026-04-04 14:55:24 +0200

    fix

--- QuizWiz-Backend/Program.cs
+++ QuizWiz-Backend/Program.cs
@@ -38,7 +38,8 @@ builder.Services.AddCors(options =>
         {
             policy.WithOrigins(allowedOrigins)
                   .AllowAnyHeader()
-                  .AllowAnyMethod();
+                  .AllowAnyMethod()
+                  .AllowCredentials();
         });
 });
 
